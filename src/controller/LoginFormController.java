828379dba08ecb1a479a1ab17b7b03b1f7873842
Janith Ranasinghe <janithraditha@gmail.com>
2024-07-09 21:21:35 +0530

    (1)create a User model and database for handle the data.(2)edit loging form controller

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void forgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage=(Stage)context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
+    }
 }
