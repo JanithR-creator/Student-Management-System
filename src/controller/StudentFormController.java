cfb9f585effb40138213c21709747edc93a2ab38
Janith Ranasinghe <janithraditha@gmail.com>
2024-07-27 15:01:06 +0530

    compele functianality for add student data to table

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,13 +1,16 @@
 package controller;
 
 import db.Database;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import model.Student;
+import view.tm.StudentTm;
 
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -18,9 +21,39 @@ public class StudentFormController {
     public DatePicker txtDOB;
     public TextField txtAddress;
     public TextField txtSearch;
+    public TableView<StudentTm> tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colDOB;
+    public TableColumn colAddress;
+    public TableColumn colOption;
 
     public void initialize() {
+
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colDOB.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
         setStudentId();
+        setTableData();
+    }
+
+    private void setTableData() {
+        ObservableList<StudentTm> obList= FXCollections.observableArrayList();
+        for(Student st:Database.studentTable){
+            Button btn = new Button("Delete");
+            StudentTm tm = new StudentTm(
+                    st.getStudentId(),
+                    st.getFullNAme(),
+                    new SimpleDateFormat("yyyy-MM-dd").format(st.getDateOfBirth()),
+                    st.getAddress(),
+                    btn
+            );
+            obList.add(tm);
+        }
+        tblStudent.setItems(obList);
     }
 
     private void setStudentId() {
@@ -49,6 +82,8 @@ public class StudentFormController {
         );
         Database.studentTable.add(student);
         setStudentId();
+        clear();
+        setTableData();
         new Alert(Alert.AlertType.INFORMATION, "Student Saved!").show();
     }
 
