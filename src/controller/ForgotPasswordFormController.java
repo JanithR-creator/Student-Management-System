54d90ce964dfc7e71d3cc36fa5e503a2a3a2589f
Janith Ranasinghe <janithraditha@gmail.com>
2024-08-01 22:04:31 +0530

    create forgot password context

---
+++ src/controller/ForgotPasswordFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ForgotPasswordFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+
+    public void backHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void sendCodeOnAction(ActionEvent actionEvent) {
+    }
+}
